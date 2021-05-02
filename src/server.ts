a43299056a2298ad9e09e227c4afb2a2a78b2a22
inzahan <inzahanyang@gmail.com>
2021-05-02 22:14:34 +0900

    apollo-server-express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
-import client from "./client";
+import * as express from "express";
+import * as logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
+import client from "./client";
 
 const PORT = process.env.PORT;
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
@@ -16,8 +18,10 @@ const server = new ApolloServer({
   },
 });
 
-server
-  .listen(PORT)
-  .then(() =>
-    console.log(`🚀 Server is running on http://localhost:${PORT}/✅`)
-  );
+const app = express();
+app.use(logger("tiny"));
+apollo.applyMiddleware({ app });
+app.use("/static", express.static("uploads"));
+app.listen({ port: PORT }, () => {
+  console.log(`🚀Server is running on http://localhost:${PORT} ✅`);
+});
